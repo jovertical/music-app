3a7ed5ee9100877ea3b40af4911ab1bac7c81c82
Jovert Palonpon <palonponjovertlota@gmail.com>
2020-02-26 02:29:19 +0800

    chore: Setup Vue Meta

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,19 @@
 /* eslint-disable no-undef */
 import Vue from 'vue'
+import VueMeta from 'vue-meta'
 import { InertiaApp } from '@inertiajs/inertia-vue'
 
 Vue.use(InertiaApp)
+Vue.use(VueMeta)
+
 Vue.prototype.$route = (...args) => route(...args).url()
 
 const app = document.getElementById('app')
 
 new Vue({
+    metaInfo: {
+        titleTemplate: title => (title ? `${title} - Music App` : 'Music App'),
+    },
     render: h =>
         h(InertiaApp, {
             props: {
